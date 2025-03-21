746ab444934062091c7bc9bc7c7ef813f68ec984
自由 <46299378+li-berty@users.noreply.github.com>
2025-03-22 00:48:30 +0300

    Add files via upload

---
+++ 1_Introduction to Programming/ex_001.py
@@ -0,0 +1,9 @@
+# Display a person’s complete mailing address.
+#
+name = "Ben Stephenson"
+department = "Department of Computer Science"
+university = "University of Calgary"
+address = "2500 University Drive NW"
+province = "Calgary, Alberta T2N 1N4"
+state = "Canada"
+print(name,'\n',department,'\n',university,'\n',address,'\n',province,'\n',state)
